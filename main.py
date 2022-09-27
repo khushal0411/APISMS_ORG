59635fd347015c41e00cd179b02e6cfd7a2976a3
Karan <karannakum123@gmail.com>
2022-09-27 20:32:06 +0530

    dataaddit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('org.csv')
+df = pd.read_csv('user.csv')
 
-with open('data.json', 'w') as f:
+with open('data_user.json', 'w') as f:
     json.dump({'Data': df.to_dict(orient='records')}, f, indent=6)
